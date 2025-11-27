9bc85fbc0cae411b589e4cd36f3a1b2d7b324a8a
Chithipan Inpaeng <chithipanbiteza555@gmail.com>
2025-11-27 11:47:19 +0700

    edi db

--- db.js
+++ db.js
@@ -3,9 +3,13 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306, // 👈 เพิ่มบรรทัดนี้สำคัญมาก!
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = pool;
